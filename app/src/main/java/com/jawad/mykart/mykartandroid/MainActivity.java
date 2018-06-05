2d1df088fe8ba97aff98121a11d472f68336f033
Jawad Nazir <jawadprogramming@gmail.com>
2018-06-05 13:08:54 +0100

    Added buttons & event handlers

--- app/src/main/java/com/jawad/mykart/mykartandroid/MainActivity.java
+++ app/src/main/java/com/jawad/mykart/mykartandroid/MainActivity.java
@@ -1,7 +1,13 @@
 package com.jawad.mykart.mykartandroid;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void addListenerOnButton(){
+        ImageButton imageButton = (ImageButton)findViewById(R.id.InfoButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+                builder.setMessage("App Version: 1.0.0\nDeveloper: Jawad Nazir").setTitle("About").setNeutralButton("Close", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.create().show();
+            }
+        });
     }
 }
